5321f185dcafcf73ac5c4c89bc33e0de5faa8f22
xiaoming <1097735900@qq.com>
2021-03-05 15:20:07 +0800

    First commit

---
+++ Library/src/main/java/com/permissionx/xiaomingdev/InvisibleFragment.kt
@@ -0,0 +1,30 @@
+package com.permissionx.xiaomingdev
+
+import android.content.pm.PackageManager
+import androidx.fragment.app.Fragment
+
+typealias PermissionCallbacK = (Boolean,List<String>) -> Unit
+
+class InvisibleFragment: Fragment() {
+    private var callback: PermissionCallbacK?=null
+
+    fun requestNow(cb: PermissionCallbacK,vararg permissions: String) {
+        callback = cb
+        requestPermissions(permissions,1)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == 1){
+            val deniedList = ArrayList<String>()
+            for ( (index,result) in grantResults.withIndex()) {
+                if (result != PackageManager.PERMISSION_GRANTED){
+                    deniedList.add(permissions[index])
+                }
+            }
+            val allGranted = deniedList.isEmpty()
+            callback?.let { it(allGranted,deniedList) }
+        }
+    }
+}
+
+typealias PermissionCallback = (Boolean, List<String>) -> Unit
